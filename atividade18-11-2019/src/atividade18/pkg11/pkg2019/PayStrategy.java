08c04329c42b241a953e1751a36498e576da7b73
Guilherme de Aguiar <gpacianotto@gmail.com>
2019-11-18 19:59:36 -0300

    criei projeto e adicionei interface

---
+++ atividade18-11-2019/src/atividade18/pkg11/pkg2019/PayStrategy.java
@@ -0,0 +1,14 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package atividade18.pkg11.pkg2019;
+
+/**
+ *
+ * @author SAMSUNG
+ */
+public interface PayStrategy {
+    
+}
